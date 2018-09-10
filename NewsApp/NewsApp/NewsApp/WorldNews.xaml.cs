e4d5e01c3cefe1ec7646c437d4723adbdef65f8f
Mikhail Frenkel <frenkel.misha@mail.ru>
2018-09-10 12:52:51 +0300

    added NewsApi packages

--- NewsApp/NewsApp/NewsApp/WorldNews.xaml.cs
+++ NewsApp/NewsApp/NewsApp/WorldNews.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +14,18 @@ namespace NewsApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class WorldNews : ContentPage
 	{
+	    private const string Key = "432f183736024ac4aa97b1975eb468ef";
+	    private NewsApiClient _newsApiClient;
+
 		public WorldNews ()
 		{
 			InitializeComponent ();
+
+		    _newsApiClient = new NewsApiClient(Key);
+		    var response = _newsApiClient.GetTopHeadlines(new TopHeadlinesRequest
+		    {
+                Language = Languages.EN
+		    });
 		}
 	}
 }
