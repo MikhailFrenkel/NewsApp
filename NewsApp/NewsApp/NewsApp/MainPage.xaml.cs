e8a6aa834dc48ddd0e2c9ee6b9a9c05179c87cc2
Mikhail Frenkel <frenkel.misha@mail.ru>
2018-09-11 09:13:18 +0300

    output result in listview

--- NewsApp/NewsApp/NewsApp/MainPage.xaml.cs
+++ NewsApp/NewsApp/NewsApp/MainPage.xaml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
 using Xamarin.Forms;
 
 namespace NewsApp
--- NewsApp/NewsApp/NewsApp/WorldNews.xaml.cs
+++ NewsApp/NewsApp/NewsApp/WorldNews.xaml.cs
@@ -14,18 +14,28 @@ namespace NewsApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class WorldNews : ContentPage
 	{
+        public ArticlesResult WorldNewsResult { get; private set; }
+
 	    private const string Key = "432f183736024ac4aa97b1975eb468ef";
 	    private NewsApiClient _newsApiClient;
 
-		public WorldNews ()
+        public WorldNews ()
 		{
 			InitializeComponent ();
-
 		    _newsApiClient = new NewsApiClient(Key);
-		    var response = _newsApiClient.GetTopHeadlines(new TopHeadlinesRequest
-		    {
-                Language = Languages.EN
-		    });
-		}
-	}
+		    GetNews();
+        }
+        private async void GetNews()
+	    {
+	        WorldNewsResult = await _newsApiClient.GetTopHeadlinesAsync(new TopHeadlinesRequest
+	        {
+	            Language = Languages.EN
+	        });
+           
+	        if (WorldNewsResult.Status == Statuses.Ok)
+	        {
+	            this.BindingContext = this;
+	        }
+	    }
+    }
 }
