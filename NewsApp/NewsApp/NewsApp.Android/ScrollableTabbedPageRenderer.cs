615e82128b640fa6448ddaae07d211411bd3c2c5
Mikhail Frenkel <frenkel.misha@mail.ru>
2018-09-13 14:48:44 +0300

    was added scrollable tabbed page renderer

---
+++ NewsApp/NewsApp/NewsApp.Android/ScrollableTabbedPageRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Support.Design.Widget;
+using NewsApp;
+using NewsApp.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(ScrollableTabbedPageRenderer))]
+namespace NewsApp.Droid
+{
+    public class ScrollableTabbedPageRenderer : TabbedPageRenderer
+    {
+        public ScrollableTabbedPageRenderer(Context context) : base(context)
+        {
+        }
+
+        public override void OnViewAdded(Android.Views.View child)
+        {
+            base.OnViewAdded(child);
+            if (child is TabLayout tabLayout)
+            {
+                tabLayout.TabMode = TabLayout.ModeScrollable;            
+            }
+        }
+    }
+}
