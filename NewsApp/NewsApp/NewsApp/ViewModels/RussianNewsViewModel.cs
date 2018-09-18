5cdde5374bbeaba884ad30d22974f30affdac747
Mikhail Frenkel <frenkel.misha@mail.ru>
2018-09-18 14:32:26 +0300

    added view model for russian news

---
+++ NewsApp/NewsApp/NewsApp/ViewModels/RussianNewsViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
+using Plugin.Connectivity;
+
+namespace NewsApp.ViewModels
+{
+    public class RussianNewsViewModel : INotifyPropertyChanged
+    {
+        private const string Key = "432f183736024ac4aa97b1975eb468ef";
+        private readonly NewsApiClient _newsApiClient;
+        private List<Article> _russianNewsArticles;
+        private State _state;
+
+        public State IsState
+        {
+            get => _state;
+            set
+            {
+                if (_state != value)
+                {
+                    _state = value;
+                    OnPropertyChanged("IsState");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<Article> RussianNewsArticles
+        {
+            get => _russianNewsArticles;
+            set
+            {
+                if (_russianNewsArticles != value)
+                {
+                    _russianNewsArticles = value;
+                    OnPropertyChanged("RussianNewsArticles");
+                }
+            }
+        }
+
+        public RussianNewsViewModel()
+        {
+            _state = State.Loading;
+            _newsApiClient = new NewsApiClient(Key);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task GetNews()
+        {
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                //todo: api get news
+                var result = await _newsApiClient.GetTopHeadlinesAsync(new TopHeadlinesRequest
+                {
+                    Q = "russia",
+                    Language = Languages.EN,
+                    PageSize = 10
+                });
+
+                if (result.Status == Statuses.Ok)
+                {
+                    if (RussianNewsArticles == null)
+                    {
+                        RussianNewsArticles = result.Articles;
+                    }
+                    else if (RussianNewsArticles.SequenceEqual(result.Articles))
+                    {
+                        RussianNewsArticles = result.Articles;
+                    }
+
+                    IsState = State.Normal;
+                }
+                else if (result.Status == Statuses.Error)
+                {
+                    IsState = State.Error;
+                }
+            }
+            else
+            {
+                if (RussianNewsArticles == null)
+                    IsState = State.NoInternet;
+            }
+        }
+    }
+}
--- NewsApp/NewsApp/NewsApp/Views/RussianNews.xaml.cs
+++ NewsApp/NewsApp/NewsApp/Views/RussianNews.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using NewsApp.ViewModels;
 using NewsAPI;
 using NewsAPI.Constants;
 using NewsAPI.Models;
@@ -17,60 +18,41 @@ namespace NewsApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class RussianNews : ContentPage
 	{
-	    public ArticlesResult RussianNewsResult { get; private set; }
-
-	    public ICommand RefreshCommand
-	    {
-	        get
-	        {
-	            return new Command(async () =>
-	            {
-	                await GetNews();
-	                NewsListView.IsRefreshing = false;
-	            });
-	        }
-	    }
-
-	    private const string Key = "432f183736024ac4aa97b1975eb468ef";
-	    private readonly NewsApiClient _newsApiClient;
+	    private RussianNewsViewModel _russianNewsViewModels;
 
 	    public RussianNews()
 	    {
 	        InitializeComponent();
-	        BindingContext = this;
-	        _newsApiClient = new NewsApiClient(Key);
+	        _russianNewsViewModels = new RussianNewsViewModel();
+	        BindingContext = _russianNewsViewModels;
 	    }
 
 	    protected override async void OnAppearing()
 	    {
 	        base.OnAppearing();
-
-            if (NewsListView.ItemsSource == null)
-	            await GetNews();
+	        if (_russianNewsViewModels.RussianNewsArticles == null)
+	            await _russianNewsViewModels.GetNews();
 	    }
-
-	    private async Task GetNews()
-	    {
-	        if (CrossConnectivity.Current.IsConnected)
-	        {
-	            RussianNewsResult = await _newsApiClient.GetTopHeadlinesAsync(new TopHeadlinesRequest
-	            {
-	                Q = "russia",
-	                Language = Languages.EN,
-	                PageSize = 10
-	            });
-
-	            if (RussianNewsResult.Status == Statuses.Ok)
-	            {
-	                NewsListView.ItemsSource = RussianNewsResult.Articles;
-	            }
-	        }
-	    }
-
 	    private async void NewsListView_OnItemTapped(object sender, ItemTappedEventArgs e)
 	    {
 	        string url = (e.Item as Article)?.Url;
 	        await Navigation.PushAsync(new BrowserPage(url));
+	    }
+
+	    private async void NewsListView_OnRefreshing(object sender, EventArgs e)
+	    {
+	        if (_russianNewsViewModels != null)
+	            await _russianNewsViewModels.GetNews();
+	        NewsListView.IsRefreshing = false;
+	    }
+
+	    private async void Button_OnClicked(object sender, EventArgs e)
+	    {
+	        if (_russianNewsViewModels != null)
+	        {
+	            _russianNewsViewModels.IsState = State.Loading;
+	            await _russianNewsViewModels.GetNews();
+	        }
 	    }
     }
 }
