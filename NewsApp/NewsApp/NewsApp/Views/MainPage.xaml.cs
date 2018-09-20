099e0d8558d0469cfbbdeb6e9f2082608e48a98f
Mikhail Frenkel <frenkel.misha@mail.ru>
2018-09-20 12:48:50 +0300

    added tabbedpage.children in c# code

--- NewsApp/NewsApp/NewsApp/Views/MainPage.xaml.cs
+++ NewsApp/NewsApp/NewsApp/Views/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NewsApp.Views;
 using NewsAPI;
 using NewsAPI.Constants;
 using NewsAPI.Models;
@@ -15,6 +16,9 @@ namespace NewsApp
         public MainPage()
         {
             InitializeComponent();
+            Children.Add(new NewsPage("World News", "world"));
+            Children.Add(new NewsPage("News in Russia", "russia"));
+            Children.Add(new NewsPage("Science news", "science"));
             NavigationPage.SetHasNavigationBar(this, false);
         }
     }
