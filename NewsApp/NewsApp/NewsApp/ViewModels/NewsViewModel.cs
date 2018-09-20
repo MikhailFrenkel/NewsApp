d5ab33b988bee3b56b48486d2f4644d8aa50d939
Mikhail Frenkel <frenkel.misha@mail.ru>
2018-09-20 12:39:49 +0300

    was added news page

---
+++ NewsApp/NewsApp/NewsApp/ViewModels/NewsViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
+using Plugin.Connectivity;
+
+namespace NewsApp.ViewModels
+{
+    public class NewsViewModel : INotifyPropertyChanged
+    {
+        private const string Key = "432f183736024ac4aa97b1975eb468ef";
+        private readonly NewsApiClient _newsApiClient;
+        private List<Article> _newsArticles;
+        private State _state;
+        private readonly string _searchQuery;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public State IsState
+        {
+            get => _state;
+            set
+            {
+                if (_state != value)
+                {
+                    _state = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public List<Article> NewsArticles
+        {
+            get => _newsArticles;
+            set
+            {
+                if (_newsArticles != value)
+                {
+                    _newsArticles = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public NewsViewModel(string searchQuery)
+        {
+            _searchQuery = searchQuery;
+            _state = State.Loading;
+            _newsApiClient = new NewsApiClient(Key);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task GetNews()
+        {
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                var result = await _newsApiClient.GetTopHeadlinesAsync(new TopHeadlinesRequest
+                {
+                    Language = Languages.EN,
+                    Q = _searchQuery,
+                    PageSize = 10
+                });
+
+                if (result.Status == Statuses.Ok)
+                {
+                    if (NewsArticles == null)
+                    {
+                        NewsArticles = result.Articles;
+                    }
+                    else if (NewsArticles.SequenceEqual(result.Articles))
+                    {
+                        NewsArticles = result.Articles;
+                    }
+
+                    IsState = State.Normal;
+                }
+                else if (result.Status == Statuses.Error)
+                {
+                    IsState = State.Error;
+                }
+            }
+            else
+            {
+                if (NewsArticles == null)
+                    IsState = State.NoInternet;
+            }
+        }
+    }
+}
---
+++ NewsApp/NewsApp/NewsApp/Views/NewsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewsApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NewsApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewsPage : ContentPage
+	{
+	    private readonly NewsViewModel _newsViewModel;
+		public NewsPage (string title, string searchQuery)
+		{
+			InitializeComponent ();
+		    Title = title;
+            _newsViewModel = new NewsViewModel(searchQuery);
+		    NewsView.SetBinding(NewsView.NewsResultProperty, new Binding() { Source = _newsViewModel, Path = "NewsArticles" });
+		    NewsView.SetBinding(NewsView.IsStateProperty, new Binding() { Source = _newsViewModel, Path = "IsState" });
+		    NewsView.GetNews = _newsViewModel.GetNews;
+        }
+
+	    protected override async void OnAppearing()
+	    {
+	        base.OnAppearing();
+	        if (_newsViewModel.NewsArticles == null)
+	            await _newsViewModel.GetNews();
+	    }
+    }
+}
