dd62f2db29c426175c07d53241afb1ccca5a0283
Mikhail Frenkel <frenkel.misha@mail.ru>
2018-10-09 13:30:06 +0300

    added splash screen

--- NewsApp/NewsApp/NewsApp.Android/MainActivity.cs
+++ NewsApp/NewsApp/NewsApp.Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace NewsApp.Droid
 {
-    [Activity(Label = "NewsApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "NewsApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- NewsApp/NewsApp/NewsApp.Android/Resources/Resource.designer.cs
+++ NewsApp/NewsApp/NewsApp.Android/Resources/Resource.designer.cs
@@ -2350,26 +2350,26 @@ namespace NewsApp.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020139
-			public const int avd_hide_password_1 = 2130837817;
-			
-			// aapt resource value: 0x7f02013a
-			public const int avd_hide_password_2 = 2130837818;
-			
 			// aapt resource value: 0x7f02013b
-			public const int avd_hide_password_3 = 2130837819;
+			public const int avd_hide_password_1 = 2130837819;
+			
+			// aapt resource value: 0x7f02013c
+			public const int avd_hide_password_2 = 2130837820;
+			
+			// aapt resource value: 0x7f02013d
+			public const int avd_hide_password_3 = 2130837821;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f02013c
-			public const int avd_show_password_1 = 2130837820;
-			
-			// aapt resource value: 0x7f02013d
-			public const int avd_show_password_2 = 2130837821;
-			
 			// aapt resource value: 0x7f02013e
-			public const int avd_show_password_3 = 2130837822;
+			public const int avd_show_password_1 = 2130837822;
+			
+			// aapt resource value: 0x7f02013f
+			public const int avd_show_password_2 = 2130837823;
+			
+			// aapt resource value: 0x7f020140
+			public const int avd_show_password_3 = 2130837824;
 			
 			// aapt resource value: 0x7f020056
 			public const int delete = 2130837590;
@@ -3022,11 +3022,11 @@ namespace NewsApp.Droid
 			// aapt resource value: 0x7f02012e
 			public const int notification_icon_background = 2130837806;
 			
-			// aapt resource value: 0x7f020137
-			public const int notification_template_icon_bg = 2130837815;
+			// aapt resource value: 0x7f020139
+			public const int notification_template_icon_bg = 2130837817;
 			
-			// aapt resource value: 0x7f020138
-			public const int notification_template_icon_low_bg = 2130837816;
+			// aapt resource value: 0x7f02013a
+			public const int notification_template_icon_low_bg = 2130837818;
 			
 			// aapt resource value: 0x7f02012f
 			public const int notification_tile_bg = 2130837807;
@@ -3038,19 +3038,25 @@ namespace NewsApp.Droid
 			public const int plus = 2130837809;
 			
 			// aapt resource value: 0x7f020132
-			public const int search = 2130837810;
+			public const int sam_solutions = 2130837810;
 			
 			// aapt resource value: 0x7f020133
-			public const int share = 2130837811;
+			public const int search = 2130837811;
 			
 			// aapt resource value: 0x7f020134
-			public const int tooltip_frame_dark = 2130837812;
+			public const int share = 2130837812;
 			
 			// aapt resource value: 0x7f020135
-			public const int tooltip_frame_light = 2130837813;
+			public const int splash_screen = 2130837813;
 			
 			// aapt resource value: 0x7f020136
-			public const int trash = 2130837814;
+			public const int tooltip_frame_dark = 2130837814;
+			
+			// aapt resource value: 0x7f020137
+			public const int tooltip_frame_light = 2130837815;
+			
+			// aapt resource value: 0x7f020138
+			public const int trash = 2130837816;
 			
 			static Drawable()
 			{
@@ -4217,8 +4223,8 @@ namespace NewsApp.Droid
 			// aapt resource value: 0x7f0c016e
 			public const int Animation_Design_BottomSheetDialog = 2131493230;
 			
-			// aapt resource value: 0x7f0c0191
-			public const int AppCompatDialogStyle = 2131493265;
+			// aapt resource value: 0x7f0c0192
+			public const int AppCompatDialogStyle = 2131493266;
 			
 			// aapt resource value: 0x7f0c00a9
 			public const int Base_AlertDialog_AppCompat = 2131493033;
@@ -4721,11 +4727,11 @@ namespace NewsApp.Droid
 			// aapt resource value: 0x7f0c000e
 			public const int CardView_Light = 2131492878;
 			
-			// aapt resource value: 0x7f0c018f
-			public const int MainTheme = 2131493263;
-			
 			// aapt resource value: 0x7f0c0190
-			public const int MainTheme_Base = 2131493264;
+			public const int MainTheme = 2131493264;
+			
+			// aapt resource value: 0x7f0c0191
+			public const int MainTheme_Base = 2131493265;
 			
 			// aapt resource value: 0x7f0c002d
 			public const int Platform_AppCompat = 2131492909;
@@ -5116,6 +5122,9 @@ namespace NewsApp.Droid
 			
 			// aapt resource value: 0x7f0c0006
 			public const int Theme_MediaRouter_LightControlPanel = 2131492870;
+			
+			// aapt resource value: 0x7f0c018f
+			public const int Theme_Splash = 2131493263;
 			
 			// aapt resource value: 0x7f0c0116
 			public const int ThemeOverlay_AppCompat = 2131493142;
